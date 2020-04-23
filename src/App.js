16d592a4af12d52bb652716fb9d139606241c8ee
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-04-22 17:33:37 -0700

    Wrote the handlerFunc to add the passed amount to the correct teams state. I then passed that into each onClick for each button.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 const [homeScore, setHomeScore] = useState(0);
 const [awayScore, setAwayScore] = useState(0);
+// This function will add the correct score to the state
+const handlerFunc = (setScore, team, amount) => {
+  return setScore(team + amount);
+}
 
   return (
     <div className="container">
@@ -38,12 +42,14 @@ const [awayScore, setAwayScore] = useState(0);
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={ () => setHomeScore(homeScore + 7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={ () => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+
+          {/* The handler function is passed into the onClick here */}
+          <button className="homeButtons__touchdown" onClick={ () => handlerFunc(setHomeScore, homeScore, 7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={ () => handlerFunc(setHomeScore, homeScore, 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={ () => setAwayScore(awayScore + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={ () => setAwayScore(awayScore + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={ () => handlerFunc(setAwayScore, awayScore, 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={ () => handlerFunc(setAwayScore, awayScore, 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
